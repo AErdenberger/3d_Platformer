aee959f3034bac123aeaf51e22961ec05cc9e8f7
Alex Erdenberger <erdenberger.alexander@gmail.com>
2024-01-02 15:41:20 -0800

    Moving Written

--- Player.gd
+++ Player.gd
@@ -9,9 +9,20 @@ var facing_angle : float
 @onready var model : MeshInstance3D = get_node("Model")
 
 func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
 	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var dir = Vector3(input.x, 0, input.y)
 	
-	velocity = dir #created and used within CharacterBody3D which we are extending from
+	velocity.x = dir.x * move_speed #created and used within CharacterBody3D which we are extending from
+	velocity.z = dir.z * move_speed
 	
 	move_and_slide()
+	
+	if input.length() > 0:
+		facing_angle = Vector2(input.y, input.x).angle()
+		#swap y and x to get y-axis angle
+	
+	model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.5) 
