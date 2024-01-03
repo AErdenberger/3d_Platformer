be9f5bdb54245ea57dae2e749afefbc614c88c78
Alex Erdenberger <erdenberger.alexander@gmail.com>
2024-01-02 16:22:40 -0800

    Jumping and Resetting

--- Player.gd
+++ Player.gd
@@ -10,19 +10,34 @@ var facing_angle : float
 
 func _physics_process(delta):
 	
+	#apply gravity if we're in the air
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
 	
+	#get keyboard input
 	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	#calculate move direction
 	var dir = Vector3(input.x, 0, input.y)
 	
+	#assign direction to velocity
 	velocity.x = dir.x * move_speed #created and used within CharacterBody3D which we are extending from
 	velocity.z = dir.z * move_speed
 	
+	#apply velocity
 	move_and_slide()
 	
+	#if we're moving, set facing direction
 	if input.length() > 0:
 		facing_angle = Vector2(input.y, input.x).angle()
 		#swap y and x to get y-axis angle
 	
 	model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.5) 
+	
+	if global_position.y < -5:
+		game_over()
+
+func game_over():
+	get_tree().reload_current_scene()
